706bd1dc3490b34eb48516e9f7704796825b2867
ShahDhruvik <devep1210@gmail.com>
2023-09-11 23:30:21 +0530

    Small changes in comments

--- src/lib/Card.ts
+++ src/lib/Card.ts
@@ -1,4 +1,5 @@
-// All the Api call that does not concern with the redux store will be defined here and imported wherever needed
+// All the Api call that does not concern with the redux store will be defined here and imported wherever needed.
+// ex: getAPI,postAPI,etc are defined here
 export const numberOfcards = () => {
     return 100
 }
